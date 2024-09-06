158a823225b7038913f9e18a74f73f7f616e47ea
ElielSilva <silvaeliel940@gmail.com>
2024-09-06 04:14:31 -0300

    [FEAT] - add login with auth0

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+import LoginButton from './components/ButtonLogin';
 
 function App() {
+  console.log(process.env.AUTH_CLIENTID)
   return (
     <div className="App">
       app
+      <p>
+        {}
+      </p>
+      <LoginButton />
     </div>
   );
 }
